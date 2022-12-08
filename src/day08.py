f4864c5df2f9d88d2f7e70c4e723da0a1a1f3a6b
Alberto Santagostino <santagostino.alb@gmail.com>
2022-12-08 14:33:30 +0100

    Add day08 part2 solution

--- src/day08.py
+++ src/day08.py
@@ -8,12 +8,13 @@ from common.meta_utils import get_puzzle_input
 
 class WoodenTree():
     """A real 🌳 (or maybe a 🌲, but most likely a 🎄, who knows?)"""
-    def __init__(self, height: int, visible: bool = False):
+    def __init__(self, height: int, visible: bool = False, score: int = 0):
         self.height = height
         self.visible = visible
+        self.score = score
 
     def __repr__(self):
-        return f"{self.height}{'V' if self.visible else 'H'}"
+        return f"{self.height}:{'V' if self.visible else 'H'}:{self.score}"
 
 
 def part1(grid):
@@ -33,12 +34,36 @@ def part1(grid):
     return sum(1 for el in grid.flat if el.visible)
 
 
+def part2(grid):
+    for (xx, yy), tree in np.ndenumerate(grid):
+        if xx != 0 and yy != 0 and xx != len(grid) - 1 and yy != len(grid) - 1:
+            grid[xx, yy].score = 1
+            for direction in [[1, 0], [-1, 0], [0, 1], [0, -1]]:
+                grid[xx, yy].score *= look_from_tree(grid, xx, yy, direction)
+    return max(el.score for el in grid.flat)
+
+
+def look_from_tree(grid, xx, yy, direction):
+    visible_trees = 0
+    this_height = grid[xx, yy].height
+    xx += direction[0]
+    yy += direction[1]
+    while True:
+        visible_trees += 1
+        if grid[xx, yy].height >= this_height:
+            break
+        xx += direction[0]
+        yy += direction[1]
+        if xx < 0 or yy < 0 or xx >= len(grid) or yy >= len(grid):
+            break
+    return visible_trees
+
+
 def look_from_boundary(grid, x, y, direction):
     max_height = -1
     while True:
-        tree = grid[x, y]
-        if tree.height > max_height:
-            max_height = tree.height
+        if grid[x, y].height > max_height:
+            max_height = grid[x, y].height
             grid[x, y].visible = True
         x += direction[0]
         y += direction[1]
